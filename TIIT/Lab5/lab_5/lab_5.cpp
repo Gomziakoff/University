f6768de254011b234cd0552ac07f294cd5695831
Gomziakoff <88232005+Gomziakoff@users.noreply.github.com>
2022-10-24 23:39:05 +0300

    Add files via upload

---
+++ TIIT/Lab5/lab_5/lab_5.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned char a[14];
+	int i, b[14];
+	int j;
+	cout << "vvedite odno chislo shtrihcoda, esli chislo propusheno vvedite ?" << endl;
+	int sr = 0, s1 = 0, sn = 0, s = 0, t = 0, p = 0 ,proverka = 0, num = 0;
+	for (int i = 0; i < 13; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < 13; i++) {
+		if (a[i] == '?') {
+			a[i] = '0';
+			num = i;
+			proverka = 1;
+		}
+	}
+	for (int i = 0; i < 13; i++) {
+		b[i] =  a[i]-'0';//я не знаю как это работает
+		cout << b[i];
+	}
+	cout << " ваш штрих-код " << endl;
+		for (int j = 0; j < 12; j++) {
+			if (j % 2 == 1)
+				sr = sr + b[j];
+		}
+		s1 = sr * 3;
+		for (int j = 0; j <= 11; j++) {
+			if (j % 2 == 0)
+				s1 += b[j];
+		}
+		s = s1 + sn;
+		t = s % 10;
+		p = 10 - t;
+		if (proverka == 0) {
+			if (p == b[12]) {
+				cout << "штрихкод верный" << endl;
+			}
+			else
+			{
+
+				cout << "штрихкод неверный "<<endl;
+				for (int i = 0; i < 13; i++) {
+					cout << b[i];
+					b[12] = p;
+				}
+				cout << " вот его правильный вариант";
+			}
+		}
+		if (proverka == 1) {
+			while (10 - s % 10 != b[12]) {
+				if (num == 12) {
+					b[12] = 10 - s % 10;
+				}
+				else if (num % 2 ) {
+						s = s + 3;
+						b[num]++;
+				}
+				else if (num % 2 == 0) {
+							s = s + 1;
+							b[num]++;
+						}
+			}
+			cout << "вот готовый штрихкод ";
+			for (int i = 0; i < 13; i++) {
+				cout << b[i];
+			}
+
+		}
+}//string
---
+++ TIIT/Lab6/lab6/lab6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	 long long int a = 4, p = 11, xi, xj,yi,yj;
+	 int zij, zji;
+	 cout << "Введите ключ ";
+	 cin >> xi;//первое секретное число
+	 yi = int(pow(a, xi)) % p;//вычисляем публичное число 1 человека
+	 cout << "Введите ключ ";
+	 cin >> xj;//второе секретное число
+	 zij = int(pow(yi, xj)) % p;//вычисляем секретный ключ 1 человека
+	 yj = int(pow(a, xj)) % p;//вычисляем публичное число 2 человека
+	 zji = int(pow(yj,xi)) % p;//вычисляем секретный ключ 2 человека
+	 cout <<"Секретный ключ 1: " << zij << endl<< " Секретный ключ 2: " << zji;
+}
+
