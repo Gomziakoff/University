4ddab1610a3d1c9d5a1b8224509f7256fcd451fe
Gomziakoff <lesha.romanyik@gmail.com>
2023-02-26 22:41:04 +0300

    git add -A

---
+++ oaip/2-semestr/Lab10/Lab10.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <iomanip>
+
+enum Continent { Europe = 1, Africa, North_America, South_America, Australia, Asia };
+
+union Sea {
+	int num;
+	char access;
+};
+
+struct Country {
+	char name[20],
+		capital[20],
+		currency[20];
+	int population;
+	Continent continent;
+	Sea sea;
+	unsigned goverment : 2;
+};
+
+int menu();                                //организация меню
+void input(Country*& array, int& N);       //ввод данных массива структуры
+void output(Country* array, int N);        //вывод данных массива структур
+void sort(Country* array, int N);          //сортировка во алфавиту по фамилии
+int search(Country* array, int N);         //поиск по заданному параметру
+void deleted(Country* array, int& N);      //удаление структуры из массива структур
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	menu();
+	return 0;
+}
+
+int menu() {
+	int choise, N = 0;
+	Country* array = new Country[N];
+	do {
+		std::cout << "Выберите действие:\n";
+		std::cout << "1) Ввод массива структур\n";
+		std::cout << "2) Вывод массива структур\n";
+		std::cout << "3) Сортировка массива структур по алфавит\n";
+		std::cout << "4) Поиск страны с наибольшим населением\n";
+		std::cout << "5) Удаление заданной структуры\n";
+		std::cout << "6) Выход\n";
+		std::cin >> choise;
+		system("cls");
+
+		switch (choise)
+		{
+		case 1:
+			input(array, N);
+			break;
+		case 2:
+			std::cin.ignore();
+			output(array, N);
+			break;
+		case 3:
+			std::cin.ignore();
+			sort(array, N);
+			output(array, N);
+			break;
+		case 4:
+			std::cin.ignore();
+			for (int i = 0; i < N; i++) {
+				if (array[i].population == search(array, N)) {
+					std::cout << "Страна с самым большим населением:\n";
+					std::cout << array[i].name << " (" << array[i].population << " человек)\n";
+				}
+			}
+			break;
+		case 5:
+			std::cin.ignore();
+			deleted(array, N);
+			break;
+		case 6: return 0;
+		}
+		std::cin.ignore();
+		system("cls");
+	} while (true);
+	delete[] array;
+}
+
+void input(Country*& array, int& N) {
+	int n;
+	std::cout << "Введите количество стран: \n";
+	std::cin >> n;
+	N = N + n;
+	Country* tempArr = new Country[N];
+	for (int i = 0; i < N - n; i++)
+		tempArr[i] = array[i];
+	array = new Country[N];
+	for (int i = 0; i < N - n; i++)
+		array[i] = tempArr[i];
+	delete tempArr;
+	system("cls");
+	std::cin.ignore();
+	for (int i = N - n; i < N; i++) {
+		int choise;
+		std::cout << "1) Название страны: ";
+		gets_s(array[i].name);
+		std::cout << "2) Столица: ";
+		gets_s(array[i].capital);
+		std::cout << "3) Валюта: ";
+		gets_s(array[i].currency);
+		std::cout << "4) Континент (1-Европа, 2-Африка, 3-Северная Америка, 4-Южная Америка, 5-Австралия, 6-Азия): ";
+		std::cin >> choise;
+		array[i].continent = (Continent)choise;
+		std::cout << "5) Выход к морю (1-есть, 2-нет): ";
+		std::cin >> array[i].sea.num;
+		if (array[i].sea.num == 1)
+			array[i].sea.access = '+';
+		if (array[i].sea.num == 2)
+			array[i].sea.access = '-';
+		std::cout << "6) Население: ";
+		std::cin >> array[i].population;
+		std::cout << "7) Форма правления (1-монархия, 2-республика): ";
+		std::cin >> choise;
+		array[i].goverment = choise;
+		std::cin.ignore();
+		system("cls");
+	}
+}
+
+void output(Country* array, int N) {
+	std::cout << std::setw(8) << std::left << "№" << std::setw(15) << std::left << "Страна" << std::setw(15) << std::left << "Столица" << std::setw(15) << std::left << "Валюта";
+	std::cout << std::setw(20) << std::left << "Континент" << std::setw(15) << std::left << "Выход к морю" << std::setw(15) << std::left << "Население" << std::setw(15) << std::left << "Форма правления\n";
+	for (int i = 0; i < N; i++) {
+		std::cout << i + 1 << "\t";
+		std::cout << std::setw(15) << std::left << array[i].name;
+		std::cout << std::setw(15) << std::left << array[i].capital;
+		std::cout << std::setw(15) << std::left << array[i].currency;
+		switch (array[i].continent) {
+		case Europe:
+			std::cout << std::setw(20) << std::left << "Европа";
+			break;
+		case Africa:
+			std::cout << std::setw(20) << std::left << "Африка";
+			break;
+		case North_America:
+			std::cout << std::setw(20) << std::left << "Северная Америка";
+			break;
+		case South_America:
+			std::cout << std::setw(20) << std::left << "Южная Америка";
+			break;
+		case Australia:
+			std::cout << std::setw(20) << std::left << "Австралия";
+			break;
+		case Asia:
+			std::cout << std::setw(20) << std::left << "Азия";
+			break;
+		}
+		std::cout << std::setw(15) << std::left << array[i].sea.access;
+		std::cout << std::setw(15) << std::left << array[i].population;
+		if (array[i].goverment == 1)
+			std::cout << std::setw(15) << std::left << "монархия";
+		if (array[i].goverment == 2)
+			std::cout << std::setw(15) << std::left << "республика";
+		std::cout << std::endl;
+	}
+}
+
+void sort(Country* array, int N) {
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = i; j < N; j++) {
+			if (strcmp(array[j].name, array[i].name) < 0)
+				std::swap(array[i], array[j]);
+		}
+	}
+}
+
+int search(Country* array, int N) {
+	int max = array[0].population, max_ind = 0;
+	for (int i = 1; i < N; i++) {
+		if (array[i].population > max)
+			max = array[i].population;
+	}
+	return max;
+}
+
+void deleted(Country* array, int& N) {
+	char Name[20];
+	std::cout << "Введите название страны, которую хотите удалить.\n";
+	gets_s(Name);
+	for (int i = 0; i < N; i++) {
+		if (strcmp(array[i].name, Name) == 0) {
+			N--;
+			for (int j = i; j < N; j++)
+				array[j] = array[j + 1];
+			i--;
+		}
+	}
+	Country* tempArr = new Country[N];
+	for (int i = 0; i < N; i++)
+		tempArr[i] = array[i];
+	array = new Country[N];
+	for (int i = 0; i < N; i++)
+		array[i] = tempArr[i];
+	delete tempArr;
+}
+
---
+++ tmois/lab1/tmois3/tmois3.cpp
@@ -0,0 +1,271 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool* ob(int* um,int u, bool* maska, bool* maskb) {
+	bool* maskc = new bool[u]{0};
+	for (int i = 0; i < u; i++) {
+		if (maska[i] || maskb[i]) {
+			maskc[i] = 1;
+		}
+	}
+	return maskc;
+}
+
+bool* peres(int* um, int u, bool* maska, bool* maskb) {
+	bool* maskc = new bool[u] {0};
+	for (int i = 0; i < u; i++) {
+		if (maska[i] && maskb[i]) {
+			maskc[i] = 1;
+		}
+	}
+	return maskc;
+}
+
+bool* razn(int* um, int u, bool* maska, bool* maskb) {
+	bool* maskc = new bool[u] {0};
+	for (int i = 0; i < u; i++) {
+		if (maska[i] && !maskb[i]) {
+			maskc[i] = 1;
+		}
+	}
+	return maskc;
+}
+
+bool* sim_razn(int* um, int u, bool* maska, bool* maskb) {
+	bool* maskc = new bool[u] {0};
+	for (int i = 0; i < u; i++) {
+		if ((maska[i] ^ maskb[i])) {
+			maskc[i] = 1;
+		}
+	}
+	return maskc;
+}
+
+bool* dopoln(int* um, int u, bool* maska) {
+	bool* maskc = new bool[u] {0};
+	for (int i = 0; i < u; i++) {
+		if (!maska[i]) {
+			maskc[i] = 1;
+		}
+	}
+	return maskc;
+}
+
+void boolean(int *am, int a)
+{
+	cout << "{ ";
+	for (int i = 0; i < pow(2,a); i++) {
+		cout << "{";
+		for (int j = 0; j < a; j++) {
+			if (i & (1 << j))
+				cout << am[j] << " ";
+		}
+		cout << "} ";
+	}
+	cout << "} \n";
+}
+
+void slianie(int *am, int a, int* bm, int b)
+{
+	int* rez = new int[a + b];
+	int i = 0, j = 0, k = 0;
+	while (i != a && j != b) {
+		if (am[i] < bm[j]) {
+			rez[k] = am[i];
+			i++;
+			k++;
+		}
+		else
+			if (bm[j] < am[i]) {
+				rez[k] = bm[j];
+				j++;
+				k++;
+			}
+			else
+				if (am[i] == bm[j]) {
+					rez[k] = am[i];
+					k++;
+					i++;
+					rez[k] = bm[j];
+					k++;
+					j++;
+				}
+	}
+	if (i == a && j == b);
+	else {
+		if (i != a)
+		{
+			while (i != a)
+			{
+				rez[k] = am[i];
+				k++;
+				i++;
+			}
+		}
+		if (j != b)
+		{
+			while (j != b)
+			{
+				rez[k] = bm[j];
+				k++;
+				j++;
+			}
+		}
+	}
+	for (int i = 0; i < a + b; i++) {
+		cout << rez[i] << " ";
+	}
+	cout << "\n";
+}
+
+int main() {
+	int u, a, b, variant, end = 0, c;
+	bool* pointer;
+		cout << "Введите размер универсума :\n ";
+		cin >> u;
+		int* um = new int[u];
+		for (int i = 0; i < u; i++) {
+			um[i] = i + 1;
+		}
+		cout << "Введите размер множества А :\n ";
+		cin >> a;
+		int* am = new int[a];
+		cout << "Введите " << a << " элементов множества А\n";
+		for (int i = 0; i < a; i++) {
+			cin >> am[i];
+		}
+		cout << "Введите размер множества B :\n";
+		cin >> b;
+		int* bm = new int[b];
+		cout << "Введите " << b << " элементов множества B\n";
+		for (int i = 0; i < b; i++) {
+			cin >> bm[i];
+		}
+		bool* maska = new bool[u] {0};
+		bool* maskb = new bool[u] {0};
+		for (int i = 0; i < u; i++) {
+			for (int j = 0; j < u; j++) {
+				if (um[i] == am[j]) maska[i] = 1;
+				if (um[i] == bm[j]) maskb[i] = 1;
+			}
+		}
+		while (!end) {
+			cout << "\n1. Изменить универсум и множества\n";
+			cout << "2. Произвести обьединение А и В \n";
+			cout << "3. Произвести пересечение А и В \n";
+			cout << "4. Произвести разность А и В\n";
+			cout << "5. Произвести симметрическую разность А и В\n";
+			cout << "6. Найти дополнение А\n";
+			cout << "7. Найти булеан множества А\n";
+			cout << "8. Произвести слияние массивов А и В\n";
+			cout << "9. Вычислить выражение 8 варианта\n";
+			cout << "10. Выход из программы\n";
+			cout << "Выберите вариант\n";
+			cin >> variant;
+			system("cls");
+			switch (variant) {
+			case 1: {
+				cout << "Введите размер универсума :\n ";
+				cin >> u;
+				int* um = new int[u];
+				for (int i = 0; i < u; i++) {
+					um[i] = i + 1;
+				}
+				cout << "Введите размер множества А :\n ";
+				cin >> a;
+				int* am = new int[a];
+				cout << "Введите " << a << " элементов множества А\n";
+				for (int i = 0; i < a; i++) {
+					cin >> am[i];
+				}
+				cout << "Введите размер множества B :\n";
+				cin >> b;
+				int* bm = new int[b];
+				cout << "Введите " << b << " элементов множества B\n";
+				for (int i = 0; i < b; i++) {
+					cin >> bm[i];
+				}
+				bool* maska = new bool[u] {0};
+				bool* maskb = new bool[u] {0};
+				for (int i = 0; i < u; i++) {
+					for (int j = 0; j < u; j++) {
+						if (um[i] == am[j]) maska[i] = 1;
+						if (um[i] == bm[j]) maskb[i] = 1;
+					}
+				}
+				break; }
+			case 2:
+				pointer = ob(um, u, maska, maskb);
+				for (int i = 0; i < u; i++) {
+					if (pointer[i]) {
+						cout << um[i] << " ";
+					}
+				}cout << endl;
+				break;
+			case 3:
+				pointer = peres(um, u, maska, maskb);
+				for (int i = 0; i < u; i++) {
+					if (pointer[i]) {
+						cout << um[i] << " ";
+					}
+				}cout << endl;
+				break;
+			case 4:
+				pointer = razn(um, u, maska, maskb);
+				for (int i = 0; i < u; i++) {
+					if (pointer[i]) {
+						cout << um[i] << " ";
+					}
+				} cout << endl;
+				break;
+			case 5:
+				pointer = sim_razn(um, u, maska, maskb);
+				for (int i = 0; i < u; i++) {
+					if (pointer[i]) {
+						cout << um[i] << " ";
+					}
+				} cout << endl;
+				break;
+			case 6:
+				pointer = dopoln(um, u, maska);
+				for (int i = 0; i < u; i++) {
+					if (pointer[i]) {
+						cout << um[i] << " ";
+					}
+				} cout << endl;
+				break;
+			case 7:
+				boolean(am, a);
+				break;
+			case 8:
+				slianie(am, a, bm, b);
+				break;
+			case 9: {
+				cout << "Введите размер множества C :\n ";
+				cin >> c;
+				int* cm = new int[c];
+				cout << "Введите " << c << " элементов множества C: \n";
+				for (int i = 0; i < c; i++) {
+					cin >> cm[i];
+				}
+				bool* maskc = new bool[u] {0};
+				for (int i = 0; i < u; i++) {
+					for (int j = 0; j < u; j++) {
+						if (um[i] == cm[j]) maskc[i] = 1;
+					}
+				}
+				pointer = peres(um, u, ob(um, u, maska, maskb), dopoln(um, u, maskc));
+				for (int i = 0; i < u; i++) {
+					if (pointer[i]) {
+						cout << um[i] << " ";
+					}
+				} cout << endl;
+				break; }
+			case 10: {
+				end = 1;
+			}
+			}
+			
+		}
+}
