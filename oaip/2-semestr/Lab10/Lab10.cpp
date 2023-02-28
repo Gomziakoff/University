7c12b02f98595f37f5c6830332101b6572001685
Gomziakoff <lesha.romanyik@gmail.com>
2023-03-01 00:21:03 +0300

    git add -A

--- oaip/2-semestr/Lab10/Lab10.cpp
+++ oaip/2-semestr/Lab10/Lab10.cpp
@@ -1,202 +1,199 @@
 ﻿#include <iostream>
+#include <string>
 #include <windows.h>
 #include <iomanip>
+using namespace std;
 
-enum Continent { Europe = 1, Africa, North_America, South_America, Australia, Asia };
+enum Proizv {Belarus = 1, Russia, Lituania, Ukraine};
 
-union Sea {
-	int num;
+union Nalichie {
+	int check;
 	char access;
 };
 
-struct Country {
-	char name[20],
-		capital[20],
-		currency[20];
-	int population;
-	Continent continent;
-	Sea sea;
-	unsigned goverment : 2;
+struct book {
+	int reg;
+	string author,
+		name,
+		izdat;
+	int year: 16;
+	unsigned pages;
+	Nalichie nal;
+	Proizv proizv;
 };
 
-int menu();                                //организация меню
-void input(Country*& array, int& N);       //ввод данных массива структуры
-void output(Country* array, int N);        //вывод данных массива структур
-void sort(Country* array, int N);          //сортировка во алфавиту по фамилии
-int search(Country* array, int N);         //поиск по заданному параметру
-void deleted(Country* array, int& N);      //удаление структуры из массива структур
 
-int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	menu();
-	return 0;
-}
 
-int menu() {
-	int choise, N = 0;
-	Country* array = new Country[N];
-	do {
-		std::cout << "Выберите действие:\n";
-		std::cout << "1) Ввод массива структур\n";
-		std::cout << "2) Вывод массива структур\n";
-		std::cout << "3) Сортировка массива структур по алфавит\n";
-		std::cout << "4) Поиск страны с наибольшим населением\n";
-		std::cout << "5) Удаление заданной структуры\n";
-		std::cout << "6) Выход\n";
-		std::cin >> choise;
-		system("cls");
-
-		switch (choise)
-		{
-		case 1:
-			input(array, N);
-			break;
-		case 2:
-			std::cin.ignore();
-			output(array, N);
-			break;
-		case 3:
-			std::cin.ignore();
-			sort(array, N);
-			output(array, N);
-			break;
-		case 4:
-			std::cin.ignore();
-			for (int i = 0; i < N; i++) {
-				if (array[i].population == search(array, N)) {
-					std::cout << "Страна с самым большим населением:\n";
-					std::cout << array[i].name << " (" << array[i].population << " человек)\n";
-				}
-			}
-			break;
-		case 5:
-			std::cin.ignore();
-			deleted(array, N);
-			break;
-		case 6: return 0;
-		}
-		std::cin.ignore();
-		system("cls");
-	} while (true);
-	delete[] array;
-}
-
-void input(Country*& array, int& N) {
+void input(book*& array, int &N) 
+{
 	int n;
-	std::cout << "Введите количество стран: \n";
-	std::cin >> n;
+	cout << "Введите количество элементов массива\n";
+	cin >> n;
 	N = N + n;
-	Country* tempArr = new Country[N];
+	book* tempArr = new book[N]{};
 	for (int i = 0; i < N - n; i++)
 		tempArr[i] = array[i];
-	array = new Country[N];
+	array = new book[N];
 	for (int i = 0; i < N - n; i++)
 		array[i] = tempArr[i];
-	delete tempArr;
+	
 	system("cls");
-	std::cin.ignore();
-	for (int i = N - n; i < N; i++) {
+	for (int i = N-n; i < N; i++) {
+		array[i].reg = i + 1;
+		cout << "1) название книги\n";
+		cin >> array[i].name;
+		cout << "2) автор книги\n";
+		cin >> array[i].author;
+		cout << "3) название издательства\n";
+		cin >> array[i].izdat;
+		cout << "4) год издания книги\n";
+		int year = 0;
+		cin >> year;
+		array[i].year = year;
+		cout << "5) количество страниц\n";
+		cin >> array[i].pages;
 		int choise;
-		std::cout << "1) Название страны: ";
-		gets_s(array[i].name);
-		std::cout << "2) Столица: ";
-		gets_s(array[i].capital);
-		std::cout << "3) Валюта: ";
-		gets_s(array[i].currency);
-		std::cout << "4) Континент (1-Европа, 2-Африка, 3-Северная Америка, 4-Южная Америка, 5-Австралия, 6-Азия): ";
-		std::cin >> choise;
-		array[i].continent = (Continent)choise;
-		std::cout << "5) Выход к морю (1-есть, 2-нет): ";
-		std::cin >> array[i].sea.num;
-		if (array[i].sea.num == 1)
-			array[i].sea.access = '+';
-		if (array[i].sea.num == 2)
-			array[i].sea.access = '-';
-		std::cout << "6) Население: ";
-		std::cin >> array[i].population;
-		std::cout << "7) Форма правления (1-монархия, 2-республика): ";
-		std::cin >> choise;
-		array[i].goverment = choise;
-		std::cin.ignore();
+		cout << "6) страна поставщик (1 - Беларусь, 2 - Россия, 3 - Латвия, 4 - Украина)\n";
+		cin >> choise;
+		array[i].proizv = (Proizv)choise;
+		cout << "7) есть ли книга в наличии? (1 - да, 2 - нет)\n";
+		cin >> array[i].nal.check;
+		if (array[i].nal.check == 1) {
+			array[i].nal.access = '+';
+		} else array[i].nal.access = '-';
 		system("cls");
 	}
 }
 
-void output(Country* array, int N) {
-	std::cout << std::setw(8) << std::left << "№" << std::setw(15) << std::left << "Страна" << std::setw(15) << std::left << "Столица" << std::setw(15) << std::left << "Валюта";
-	std::cout << std::setw(20) << std::left << "Континент" << std::setw(15) << std::left << "Выход к морю" << std::setw(15) << std::left << "Население" << std::setw(15) << std::left << "Форма правления\n";
+void output(book* array, int N) 
+{
 	for (int i = 0; i < N; i++) {
-		std::cout << i + 1 << "\t";
-		std::cout << std::setw(15) << std::left << array[i].name;
-		std::cout << std::setw(15) << std::left << array[i].capital;
-		std::cout << std::setw(15) << std::left << array[i].currency;
-		switch (array[i].continent) {
-		case Europe:
-			std::cout << std::setw(20) << std::left << "Европа";
+		cout << "№" << array[i].reg << "; ";
+		cout << "Название: " << array[i].name << "; ";
+		cout << "Автор: " << array[i].author << "; ";
+		cout << "Издательство: " << array[i].izdat << "; ";
+		cout << "Год выпуска: " << array[i].year << "; ";
+		cout << "Кол-во страниц: " << array[i].pages << "; ";
+		switch (array[i].proizv) {
+		case Belarus:
+			cout << "Поставщик: " << "Беларусь" << "; ";
 			break;
-		case Africa:
-			std::cout << std::setw(20) << std::left << "Африка";
+		case Russia:
+			cout << "Поставщик: " << "Россия" << "; ";
 			break;
-		case North_America:
-			std::cout << std::setw(20) << std::left << "Северная Америка";
+		case Lituania:
+			cout << "Поставщик: " << "Литва" << "; ";
 			break;
-		case South_America:
-			std::cout << std::setw(20) << std::left << "Южная Америка";
-			break;
-		case Australia:
-			std::cout << std::setw(20) << std::left << "Австралия";
-			break;
-		case Asia:
-			std::cout << std::setw(20) << std::left << "Азия";
+		case Ukraine:
+			cout << "Поставщик: " << "Украина" << "; ";
 			break;
 		}
-		std::cout << std::setw(15) << std::left << array[i].sea.access;
-		std::cout << std::setw(15) << std::left << array[i].population;
-		if (array[i].goverment == 1)
-			std::cout << std::setw(15) << std::left << "монархия";
-		if (array[i].goverment == 2)
-			std::cout << std::setw(15) << std::left << "республика";
-		std::cout << std::endl;
+		cout << "Есть в наличии: " << array[i].nal.access << "; ";
+		cout << endl;
 	}
 }
 
-void sort(Country* array, int N) {
+void sort(book*& array, int N) //sort by year
+{
 	for (int i = 0; i < N - 1; i++) {
-		for (int j = i; j < N; j++) {
-			if (strcmp(array[j].name, array[i].name) < 0)
-				std::swap(array[i], array[j]);
+		for (int j = i + 1; j < N; j++) {
+			if (array[i].year > array[j].year) 
+				swap(array[i], array[j]);
 		}
 	}
 }
 
-int search(Country* array, int N) {
-	int max = array[0].population, max_ind = 0;
-	for (int i = 1; i < N; i++) {
-		if (array[i].population > max)
-			max = array[i].population;
+void search(book* array, int N) //search books after X year
+{
+	int find;
+	cout << "Введите книги с какого года вам показать?\n";
+	cin >> find;
+	for (int i = 0; i < N; i++) {
+		if (array[i].year >= find) {
+			cout << "№" << array[i].reg << "; ";
+			cout << "Название: " << array[i].name << "; ";
+			cout << "Автор: " << array[i].author << "; ";
+			cout << "Издательство: " << array[i].izdat << "; ";
+			cout << "Год выпуска: " << array[i].year << "; ";
+			cout << "Кол-во страниц: " << array[i].pages << "; ";
+			switch (array[i].proizv) {
+			case Belarus:
+				cout << "Поставщик: " << "Беларусь" << "; ";
+				break;
+			case Russia:
+				cout << "Поставщик: " << "Россия" << "; ";
+				break;
+			case Lituania:
+				cout << "Поставщик: " << "Литва" << "; ";
+				break;
+			case Ukraine:
+				cout << "Поставщик: " << "Украина" << "; ";
+				break;
+			}
+			cout << "Есть в наличии: " << array[i].nal.access << "; ";
+			cout << endl;
+		}
 	}
-	return max;
 }
 
-void deleted(Country* array, int& N) {
-	char Name[20];
-	std::cout << "Введите название страны, которую хотите удалить.\n";
-	gets_s(Name);
+void deleted(book* array, int& N) {
+	int find;
+	output(array, N);
+	cout << "Введите номер книги которую нужно удалить: ";
+	cin >> find;
 	for (int i = 0; i < N; i++) {
-		if (strcmp(array[i].name, Name) == 0) {
+		if (array[i].reg == find) {
 			N--;
 			for (int j = i; j < N; j++)
 				array[j] = array[j + 1];
 			i--;
 		}
+		break;
 	}
-	Country* tempArr = new Country[N];
-	for (int i = 0; i < N; i++)
-		tempArr[i] = array[i];
-	array = new Country[N];
-	for (int i = 0; i < N; i++)
-		array[i] = tempArr[i];
-	delete tempArr;
 }
 
+int menu()
+{
+	int choise = 0, N = 0;
+	book *array = new book[N];
+	do {
+		cout << "Выберите действие:\n";
+		cout << "1) Ввод массива структур\n";
+		cout << "2) Вывод массива структур\n";
+		cout << "3) Сортировка книг по году выпуска \n";
+		cout << "4) Вывести список книг с фамилиями авторов, изданных после заданного года.\n";
+		cout << "5) Удаление заданной структуры\n";
+		cout << "6) Выход\n";
+		cin >> choise;
+		system("cls");
+		
+		switch (choise) {
+		case 1:
+			input(array, N);
+			break;
+		case 2:
+			output(array, N);
+			break;
+		case 3:
+			sort(array, N);
+			system("cls");
+			output(array, N);
+			break;
+		case 4:
+			search(array, N);
+			break;
+		case 5:
+			deleted(array, N);
+			break;
+		case 6:
+			return 0;
+			break;
+		}
+	} while (true);
+
+}
+int main()
+{
+	menu();
+	return 0;
+}
