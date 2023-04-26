9c8de74e61ade62b058d15996473f916c38d7c68
Gomziakoff <lesha.romanyik@gmail.com>
2023-04-26 16:56:00 +0300

    A

--- tmois/lab3/lab3.cpp
+++ tmois/lab3/lab3.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const int INF = INT_MAX;
+const int INF = 999999;
 vector<pair<int, int>> smej[1000];
 int dist[1000]{};
 
@@ -27,31 +27,63 @@ void dekstra(int start) {
 			}
 		}
 	}
-
 }
 
-int main() {
-	cout << "введите кол-во вершин графа, кол-во ребер и стартовую вершину\n";
-	int v, r, start;
-	cin >> v >> r >> start;
 
+
+int main() {
+	cout << "введите кол-во вершин графа, кол-во ребер\n";
+	int v, r, start;
+	cin >> v >> r;
+	int graph[100][100];
 	for (int i = 1; i <= v; ++i) {
 		dist[i] = INF;
+		for (int j = 1; j <= v; j++) {
+			graph[i][j] = INF;
+			if (i == j) graph[i][j] = 0;
+		}
 	}
 	cout << "введите вершины и вес ребра\n";
 	for (int i = 1; i <= r;i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
 		smej[u].push_back(make_pair(w, v));
+		graph[u][v] = w;
 	}
-
-	dekstra(start);
-
-	for (int i = 1; i <= v; ++i) {
-		if (dist[i] == INF) {
-			cout << "INF" << endl;
-			continue;
+	int choise;
+	cout << "1.deikstra\n2.orshala\n";
+	cin >> choise;
+	
+	switch (choise) {
+	case 1:
+		cout << "введите стартовую точку";
+		cin >> start;
+		dekstra(start);
+		for (int i = 1; i <= v; ++i) {
+			if (dist[i] == INF) {
+				cout << "INF" << endl;
+				continue;
+			}
+			cout << "x" << i << " = " << dist[i] << endl;
 		}
-		cout <<"x"<<i<<" = "<< dist[i] << endl;
+		break;
+	case 2:
+		for (int k = 1; k <= v; k++) {
+			for (int i = 1; i <= v; i++) {
+				for (int j = 1; j <= v; j++) {
+					if (graph[i][j] > graph[i][k] + graph[k][j]) {
+						graph[i][j] = graph[i][k] + graph[k][j];
+					}
+				}
+			}
+		}
+		for (int i = 1; i <= v; i++) {
+			for (int j = 1; j <= v; j++) {
+				if (i == j) continue;
+				cout << i << " -> " << j << " = " << graph[i][j]<<endl;
+			}
+		}
+		break;
 	}
+	
 }
