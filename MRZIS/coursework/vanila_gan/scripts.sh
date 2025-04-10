4fa17c7e510711d81281278bf4ae9b441ca17d2c
Romaniuk Aliaksei Petrovich <lesha.romanyik@gmail.com>
2025-04-10 23:52:27 +0300

    add 3 of 5 models in coursework

---
+++ MRZIS/coursework/vanila_gan/scripts.sh
@@ -0,0 +1,4 @@
+python main.py --dataset mnist
+python main.py --dataset fashionmnist
+python main.py --dataset usps --image_size 16
+python main.py --dataset anime --image_size 64 --n_channels 3 --batch_size 64
