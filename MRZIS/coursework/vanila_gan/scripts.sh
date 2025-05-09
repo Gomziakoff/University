bc79a3ba5b53112f22b57c70bae287339dbdc682
Romaniuk Aliaksei Petrovich <88232005+Gomziakoff@users.noreply.github.com>
2025-05-09 17:10:06 +0300

    upd

---
+++ MRZIS/coursework/vanila_gan/scripts.sh
@@ -0,0 +1,5 @@
+python main.py --dataset mnist
+python main.py --dataset fashionmnist
+python main.py --dataset usps --image_size 16
+python main.py --dataset anime --image_size 64 --n_channels 3 --batch_size 64
+python main.py --dataset anime --n_channels 3 --image_size 64 --experiment_name "vanilla_gan_test_batch_128" --model_path "./model/test_batch_128" --batch_size 128 --output_path "./outputs/test_batch_128" --epochs 100
